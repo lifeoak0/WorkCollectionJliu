d622950d0f82f365886ce46c1dca6c2d29c34465
JLiu <107297778+lifeoak0@users.noreply.github.com>
2024-02-04 22:40:27 +0800

    Update scheduletable.py

--- scheduletable.py
+++ scheduletable.py
@@ -1,7 +1,6 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+import pandas as pd  #生成一个A4大小的6×6表格
+import matplotlib.pyplot as plt    #使用pandas、matplotlib库进行绘制
 from pandas.plotting import table
-
 # 创建一个6x6的空DataFrame，并用空字符串初始化所有值
 df = pd.DataFrame("", index=range(1, 7), columns=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'])
 
@@ -11,12 +10,10 @@ plt.figure(figsize=(29.7*0.393701, 21*0.393701))
 # 绘制表格
 ax = plt.gca()
 ax.axis('off')
-tbl = table(ax, df, loc='center', cellLoc='center', bbox=[0, 0, 1, 1])  # bbox用于控制表格的位置和大小
+tbl = table(ax, df, loc='center', cellLoc='center', bbox=[0, 0, 1, 1])  
 
-# 调整表格样式，使其更适合填充整个页面
 tbl.auto_set_font_size(False)
 tbl.set_fontsize(12)
-tbl.scale(1.2, 1.2)  # 根据需要调整表格的缩放比例
+tbl.scale(1.2, 1.2)  
 
-# 显示图表
 plt.show()
